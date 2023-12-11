6448afea37d1db48f7ae08855d0e3e0c70bbb63b
mhuseynn <hmehdiyev2005@gmail.com>
2023-12-11 21:48:47 +0400

    first

---
+++ Decorator/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Decorator
+{
+    public interface DataSource
+    {
+        public void writeData(string data);
+        public void readData();
+    }
+
+    public class FileDataSource : DataSource
+    {
+        private string filename;
+
+        public FileDataSource(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public void readData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void writeData(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public abstract class  DataSourceDecorator : DataSource
+    {
+
+        private DataSource wrappee;
+        public DataSourceDecorator(DataSource wrappee)
+        {
+
+            this.wrappee = wrappee;
+
+        }
+
+
+        public void readData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void writeData(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public class EncryptionDecorator : DataSourceDecorator
+    {
+        public EncryptionDecorator(DataSource wrappee) : base(wrappee)
+        {
+        }
+
+        public void readData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void writeData(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CompressionDecorator : DataSourceDecorator
+    {
+        public CompressionDecorator(DataSource wrappee) : base(wrappee)
+        {
+        }
+
+        public void readData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void writeData(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
